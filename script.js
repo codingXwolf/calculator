1a354bae984a78f592b1ef14ab6b2b1cedfed026
Jaime Ortiz <jxme88@gmail.com>
2020-10-30 20:37:40 -0700

    Added add, subtract, mulitply, and divide functions

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function(input1,input2) {
+    return input1 + input2;
+}
+
+const subtract = function(input1, input2) {
+    return input1 - input2
+}
+
+const multiply = function(input1, input2) {
+    return input1 * input2;
+}
+
+const divide = function(input1, input2) {
+    return input1 / input2; 
+}
+
