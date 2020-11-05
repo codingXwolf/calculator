370298e19f100c93198ea79c74a05f1ff9b2efa0
Jaime Ortiz <jxme88@gmail.com>
2020-11-04 21:47:30 -0800

    Test number buttons.

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const updateTotalDisplay = function(sum) {
     document.write(total);
 }
 
+const buttonPress = document.querySelector('.button');
+buttonPress.addEventListener('click', (e) => {
+    console.log(e.target.value);
+})
+
 
 const add = function(input1,input2) {
     updateTotalDisplay(input1 + input2)
