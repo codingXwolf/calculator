b6490f4dfb12d54f3156594c6488ac1dd86580c2
Jaime Ortiz <jxme88@gmail.com>
2020-11-07 13:02:45 -0800

    Aligned grid-items. Style is not final

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let total = 0;
-const updateTotalDisplay = function(sum) {
-    document.write(total);
-}
+// let total = 0;
+// const updateTotalDisplay = function(sum) {
+//     document.write(total);
+// }
 
-const buttonPress = document.querySelector('.button');
-buttonPress.addEventListener('click', (e) => {
-    console.log(e.target.value);
-})
+// const buttonPress = document.querySelector('.button');
+// buttonPress.addEventListener('click', (e) => {
+//     console.log(e.target.value);
+// })
 
 
-const add = function(input1,input2) {
-    updateTotalDisplay(input1 + input2)
-    return input1 + input2;
-}
+// const add = function(input1,input2) {
+//     updateTotalDisplay(input1 + input2)
+//     return input1 + input2;
+// }
 
-const subtract = function(input1, input2) {
+// const subtract = function(input1, input2) {
     
-    return input1 - input2
-}
+//     return input1 - input2
+// }
 
-const multiply = function(input1, input2) {
-    return input1 * input2;
-}
+// const multiply = function(input1, input2) {
+//     return input1 * input2;
+// }
 
-const divide = function(input1, input2) {
-    return input1 / input2; 
-}
+// const divide = function(input1, input2) {
+//     return input1 / input2; 
+// }
 
